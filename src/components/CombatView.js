fb728e80985b69c61364c527dd3ab50d7415d70d
Tim <tim.auldridge@outlook.com>
2020-09-28 17:23:36 -0700

    basic working combat list; needs styles and icons

--- src/components/CombatView.js
+++ src/components/CombatView.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {
   View,
   StyleSheet,
@@ -10,25 +10,25 @@ import {
   ActivityIndicator,
   TouchableOpacity,
 } from 'react-native';
+
+import {useSelector} from 'react-redux';
 import ListItem from './ListItem.js';
 
 import Styles from '../../Style';
 
 const baseUrl = 'http://dnd5eapi.co';
 
-const CombatView = ({navigation, renderItem, combatants}) => {
+const CombatView = ({navigation, renderItem}) => {
   //mock data:
   const [index, setIndex] = useState();
-  const combatants = useSelector((state) => {
-    state.combatants;
-  });
+  const combatants = useSelector((state) => state.combatants);
 
   return (
     <View style={Styles.container}>
       <Text style={Styles.defaultText}>This is the combat view</Text>
       <FlatList
         data={combatants}
-        renderItem={renderItem}
+        renderItem={({item}) => <Text>{item.name}</Text>}
         keyExtractor={(item) => item.id}
         ListEmptyComponent={() => <Text>{'<no mobs yet>'}</Text>}
         keyboardShouldPersistTaps="always"
